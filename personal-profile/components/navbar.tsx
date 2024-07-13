4f577049d557a6cfbf996f8664d608f000bbc8eb
Sheng Huang <shengh@mit.edu>
2024-07-13 18:54:06 -0400

    new website start

---
+++ personal-profile/components/navbar.tsx
@@ -0,0 +1,68 @@
+import {
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarBrand,
+  NavbarItem,
+} from "@nextui-org/navbar";
+import { Link } from "@nextui-org/link";
+import { link as linkStyles } from "@nextui-org/theme";
+import NextLink from "next/link";
+import clsx from "clsx";
+import { GiDove } from "react-icons/gi";
+import { FaLinkedin } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
+import { siteConfig } from "@/config/site";
+import { ThemeSwitch } from "@/components/theme-switch";
+
+export const Navbar = () => {
+  return (
+    <NextUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <GiDove />
+            <p className="font-bold text-inherit">Sheng</p>
+          </NextLink>
+        </NavbarBrand>
+        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <NextLink
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium",
+                )}
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden sm:flex gap-2">
+          <Link
+            isExternal
+            aria-label="Linkedin"
+            href={siteConfig.links.linkedin}
+          >
+            <FaLinkedin />
+          </Link>
+
+          <Link isExternal aria-label="Github" href={siteConfig.links.email}>
+            <MdEmail />
+          </Link>
+
+          <ThemeSwitch />
+        </NavbarItem>
+      </NavbarContent>
+    </NextUINavbar>
+  );
+};
